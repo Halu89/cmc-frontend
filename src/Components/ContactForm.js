547f9eb38218a107d3999b4ed32fddc70e23220f
Corentin Briand <corentin.briand@gmail.com>
2020-10-18 14:03:26 +0200

    add error messages on form validation

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -13,6 +13,33 @@ class ContactForm extends Component {
       touched: {},
       errors: {},
     };
+    this.validate = {
+      name: (value) => {
+        if (!exists(value)) {
+          return `Veuillez indiquer un nom`;
+        } else if (!isMinLength(value, 3)) {
+          return "Le nom doit faire plus de 3 caractères";
+        }
+      },
+      email: (value) => {
+        if (!exists(value)) {
+          return "Veuillez indiquer un mail pour la réponse";
+        }
+        if (!isValidEmail(value)) {
+          return "Veuillez utiliser une adress mail valide";
+        }
+      },
+      objet: (value) => {
+        if (!exists(value)) {
+          return `Veuillez indiquer un objet`;
+        }
+      },
+      message: (value) => {
+        if (!exists(value)) {
+          return `Veuillez remplir ce champ`;
+        }
+      },
+    };
   }
 
   handleBlur = (e) => {
@@ -20,9 +47,11 @@ class ContactForm extends Component {
     this.setState((prevState) => ({
       touched: { ...prevState.touched, [name]: true },
     }));
+    this.setState((prevState) => ({
+      errors: { ...prevState.errors, [name]: this.validate[name](value) },
+    }));
   };
 
-  validate = {};
   handleSubmit = (e) => {
     e.preventDefault();
     // Validate the form
@@ -45,6 +74,7 @@ class ContactForm extends Component {
   };
 
   render() {
+    const { values, touched, errors } = this.state;
     return (
       <div className="contact-form">
         <form onSubmit={this.handleSubmit}>
@@ -54,40 +84,53 @@ class ContactForm extends Component {
               type="text"
               name="name"
               id="name"
+              value={values.name}
               onChange={this.handleInputChange}
               onBlur={this.handleBlur}
               required
             />
-            <span></span>
+            <span>{touched.name && errors.name}</span>
           </label>
-          <label htmlFor="email">Email :</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            onChange={this.handleInputChange}
-            onBlur={this.handleBlur}
-            required
-          />
-          <label htmlFor="objet">Objet :</label>
-          <input
-            type="text"
-            name="objet"
-            id="objet"
-            onChange={this.handleInputChange}
-            onBlur={this.handleBlur}
-            required
-          />
-          <label htmlFor="message">Message :</label>
-          <textarea
-            name="message"
-            id="message"
-            onChange={this.handleInputChange}
-            onBlur={this.handleBlur}
-            rows="12"
-            cols="40"
-            required
-          />
+          <label htmlFor="email">
+            Email :
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={values.email}
+              onChange={this.handleInputChange}
+              onBlur={this.handleBlur}
+              required
+            />
+            <span>{touched.email && errors.email}</span>
+          </label>
+          <label htmlFor="objet">
+            Objet :
+            <input
+              type="text"
+              name="objet"
+              id="objet"
+              value={values.objet}
+              onChange={this.handleInputChange}
+              onBlur={this.handleBlur}
+              required
+            />
+            <span>{touched.objet && errors.objet}</span>
+          </label>
+          <label htmlFor="message">
+            Message :
+            <textarea
+              name="message"
+              id="message"
+              value={values.message}
+              onChange={this.handleInputChange}
+              onBlur={this.handleBlur}
+              rows="12"
+              cols="40"
+              required
+            />
+            <span>{touched.message && errors.message}</span>
+          </label>
           <button type="submit">Envoyer</button>
         </form>
       </div>
@@ -96,3 +139,28 @@ class ContactForm extends Component {
 }
 
 export default ContactForm;
+
+function exists(input) {
+  if (input.trim().length > 0) {
+    return true;
+  }
+  return false;
+}
+
+function isMinLength(input, min) {
+  if (input.length > min) {
+    return `${input} doit faire plus de 3 caractères`;
+  }
+  return false;
+}
+
+function isValidEmail(input) {
+  if (
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      input
+    )
+  ) {
+    return `Veuillez utiliser une adresse valide.`;
+  }
+  return false;
+}
