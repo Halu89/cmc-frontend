390bc1c786a6c3ad304b348d45007a7f5533abb9
Corentin <corentin.briand@gmail.com>
2021-05-16 15:58:54 +0200

    add responsive navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,31 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.jpg";
 import RainbowText from './RainbowText'
 
 const Header = () => {
+  const [isActive, setIsActive] = useState(false)
+  const handleClick = () => setIsActive(!isActive);
+  const closeMobileMenu = () => setIsActive(false);
+
   return (
     <nav>
       <div id="logo">
         <img src={logo} alt="Logo" width="150px" height="100%"/>
       </div>
-      <ul>
-        <li>
+      <ul className={isActive ? "active" : ""}>
+        <li onClick={closeMobileMenu}>
           <NavLink exact to="/" activeClassName="selected">
             Présentation
           </NavLink>
         </li>
-        <li>
+        <li onClick={closeMobileMenu}>
           <NavLink exact to="/lecons" activeClassName="selected">
             Leçons
           </NavLink>
         </li>
-        <li>
+        <li onClick={closeMobileMenu}>
           <NavLink exact to="/actus" activeClassName="selected">
             Actus
           </NavLink>
         </li>
       </ul>
+      <div className="mobile-menu">
+        <button onClick={handleClick}>{isActive ? "X" : "Menu"}</button>
+      </div>
     </nav>
   );
 };
