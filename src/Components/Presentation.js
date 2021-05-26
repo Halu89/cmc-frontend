d715d01388c64519fe42b5212663ab49ecceff86
Corentin <corentin.briand@gmail.com>
2021-05-26 14:47:46 +0200

    add paragraph to presentation

--- src/Components/Presentation.js
+++ src/Components/Presentation.js
@@ -11,6 +11,8 @@ const Presentation = () => {
           Au travers de l'apprentissage de la musique, aider les jeunes à se
           découvrir, s'épanouir et s'intégrer à la vie de leur communauté, en
           participant à des concerts, auditions, fêtes du village, cérémonies.
+        </p>
+        <p className="presentation-paragraph">
           Initiation musicale sous forme d’ateliers (initiation musicale pour
           les 5 ans, éveil musical pour les CP/CE1, formation musicale,
           orchestre à vents et batterie, ensemble instrumentaux, chorale
